c8df309d781365a481746392a7ad4205cc72c32f
Keks <keks@htmlacademy.ru>
2023-08-23 21:00:34 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/render-gallery.js
@@ -0,0 +1,24 @@
+import {renderThumbnails} from './render-thumbnails.js';
+import {showModalMedia} from './modal-media.js';
+
+const container = document.querySelector('.pictures');
+
+export const renderGallery = function (arrayMedia) {
+  renderThumbnails(arrayMedia);
+
+  container.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-thumbnail-id]');
+
+    if (!thumbnail) {
+      return;
+    }
+
+    evt.preventDefault();
+
+    const elementMedia = arrayMedia.find(
+      (item) => item.id === +thumbnail.dataset.thumbnailId
+    );
+
+    showModalMedia(elementMedia);
+  });
+};
