fbf5e1b13136667f70d798c3298d5fddaf311165
Keks <keks@htmlacademy.ru>
2023-08-24 23:22:53 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/modal-media.js
+++ 9/js/modal-media.js
@@ -1,10 +1,11 @@
 import {isEscapeKey} from './helpers/test-keys.js';
 import {renderFullSizeMedia} from './render-full-size-media.js';
+import {showModal, hideModal} from './modal.js';
+import {addOnButtonCloseClick} from './helpers/event-listeners.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
-const body = document.querySelector('body');
 const buttonClose = document.querySelector('.big-picture__cancel');
 
 const onDocumentKeydown = (evt) => {
@@ -21,30 +22,21 @@ const onDocumentClick = (evt) => {
   }
 };
 
-function hideModalMedia () {
-  bigPicture.classList.add('hidden');
+export function hideModalMedia () {
+  hideModal(bigPicture);
   socialCommentCount.classList.remove('hidden');
   commentsLoader.classList.remove('hidden');
-  body.classList.remove('modal-open');
-
   document.removeEventListener('keydown', onDocumentKeydown);
   document.removeEventListener('click', onDocumentClick);
-  buttonClose.removeEventListener('click', () => {
-    hideModalMedia();
-  });
+  addOnButtonCloseClick(buttonClose, hideModalMedia, false);
 }
 
 export function showModalMedia (arrayMedia) {
-  bigPicture.classList.remove('hidden');
+  showModal(bigPicture);
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
-  body.classList.add('modal-open');
-
   document.addEventListener('keydown', onDocumentKeydown);
   document.addEventListener('click', onDocumentClick);
-  buttonClose.addEventListener('click', () => {
-    hideModalMedia();
-  });
-
+  addOnButtonCloseClick(buttonClose, hideModalMedia);
   renderFullSizeMedia(arrayMedia);
 }
